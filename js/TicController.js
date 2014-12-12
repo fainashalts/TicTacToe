c3f06bf9662ec8677d593eae8390f3018e4e82e4
fsp712 <fainashalts@gmail.com>
2014-12-12 14:06:47 -0800

    player turns function working, still need to disable player whose turn it isn't

--- js/TicController.js
+++ js/TicController.js
@@ -8,16 +8,16 @@ TicController.$inject = ['$firebase'];
 
 function TicController($firebase) {
     var self= this; 
-
-    self.turns = turns;
+    self.gameName = "TTT";
+    // self.turns = turns;
     self.pickBox = pickBox;
-    self.player = player;
+
     self.winner = winner;   
-    // self.owner = owner;
+   
 
     self.board = getBoxes();
-    // self.board= board
-    // self.board = board;
+    self.clearBoard = clearBoard;
+
 
     function getBoxes() {
         var sync = new Firebase("https://tictactech.firebaseio.com/gameBoard");
@@ -26,21 +26,12 @@ function TicController($firebase) {
         
         board.$loaded().then(function(){
             setBoard();
-            // board.$add({owner:""});
-            // board.$add({owner:""});
-            // board.$add({owner:""});
-            // board.$add({owner:""});
-            // board.$add({owner:""});
-            // board.$add({owner:""});
-            // board.$add({owner:""});
-            // board.$add({owner:""});
-            // board.$add({owner:""});
-            // console.log(board);
+
         });
         
         return board;
 
-        // self.board = board;
+     
     }
 
     function setBoard(){
@@ -49,60 +40,22 @@ function TicController($firebase) {
             self.board.$add({index: i, owner: ""});
         };
     }
-
-    // getBoxes();
-
-    // function getBoxes () {
     
-    //     board.boxes = [ {box:0, owner:""}, {box:1, owner:""}, {box:2, owner:""}, {box:3, owner:""}, {box:4, owner:""}, {box:5, owner:""}, {box:6, owner:""}, {box:7, owner:""}, {box:8, owner:""}];
-    //     // var board = sync.$asArray()
-    //     board.boxes.$save();
-    //     return board; 
-    // }
+    self.gamePlay = getGamePlay(); 
+    function getGamePlay() {
+        var sync = new Firebase("https://tictactech.firebaseio.com/gamePlay");
+        var gamePlay = $firebase(sync).$asObject();
 
-    // getBoxes();
+        gamePlay.$loaded().then(function() {
+            console.log("connected!");
+            // gamePlay.turns = pickBox();
+            // gamePlay.$save();
+        })
 
-    // function getBoxes () {
-    // boxes.$save({box:0, owner:""});
-    // boxes.$save({box:1, owner:""});
-    // boxes.$save({box:2, owner:""});
-    // boxes.$save({box:3, owner:""});
-    // boxes.$save({box:4, owner:""});
-    // boxes.$save({box:5, owner:""});
-    // boxes.$save({box:6, owner:""});
-    // boxes.$save({box:7, owner:""});
-    // boxes.$save({box:8, owner:""});
-
-    // // boxes.$save(boxes);
-    // self.boxes= boxes;
-    // self.sync= sync;
-
-    // }
+        return gamePlay;
+    }
 
     
-    // getBoxes();
-   
-
-
-    // self.playerNumber= null; 
-    // self.room = getTicTacTech();
-
-    // self.room.$loaded(function(){
-    //     if(!self.room.numPlayers) {
-    //         self.room.numPlayers=0;
-    //         self.room.playerTurn=0;
-    //         self.room.$save;
-    //     }
-    //     self.playerNumber = self.room.numPlayers;
-    //     self.room.numPlayers +=1
-    //     self.room.$save();
-    // });
-
-    // function getTicTacTech () {
-    //     var ref= new Firebase("https://tictactech.firebaseio.com/room");
-    //     var TicTacTech = $firebase(ref).$asObject();
-    //     return TicTacTech;
-    // }
 
     // function getLobby () { 
   
@@ -119,30 +72,17 @@ function TicController($firebase) {
     // getLobby();
 
 
-    // angular.extend( self.lobby, 
-    //     {
-    //         name: "TicTacTech", 
-    //         numPlayer: 0
-    //     }); 
-    // self.lobby.$save();
-    // self.ref = ref;
-    // self.lobby = lobby;
-    // self.lobby.$save();
+ 
 
 
   
 
-    // self.ref = ref
-    // self.lobby = lobby 
-    // self.sync2 = sync2;
-    // self.lobby= lobby;
 
-    // self.getLobby= getLobby;
 
 
     // var turns= 1;
 
-    // function playerTurn($index) {
+    // function playerTurn() {
     //     for (var i= 1; i<3; i++) {
     //         if (!self.lobby.numPlayers){
     //             self.lobby.numPlayers = 0;
@@ -154,21 +94,7 @@ function TicController($firebase) {
     //         self.room.$save();
     //     }
 
-    // turns++;
-
-    //     if (self.boxes[$index].owner === "") {
-    //         if (turns % 2 === 0) {
-    //             return self.boxes[$index].owner = "X";
-    //         }
-    //         else {
-    //             return self.boxes[$index].owner = "O";
-    //         }
-    //     }
-    //     else {
-    //         window.alert("Now now, you know you can't have that space! Pick another!");
-    //     }
-    // }
-
+   
     // var sync = $firebase(new Firebase("https://tictactech.firebaseio.com/playerTurn"));
     // var playerTurn = sync.$asObject();
 
@@ -178,100 +104,165 @@ function TicController($firebase) {
     // }
 
     // self.getPlayer= getPlayer;
+    // function playerTurn (){
+    //     var ref = new Firebase("https://tictactech.firebaseio.com/player");
+    //     var player = $firebase(ref); 
 
+    //     function counter () {
+    //         if (player=1) {
+    //         player++;
+    //        }
+    //        else {
+    //         player--;
+    //        }
+    //     };
+       
+    //     box.player.$save(box);
+           
+    //     return player;
+    //        // self.box.player.$save();
+
+    // };
 
     
-    var turns = 1;
-    function pickBox(box) {
-            
-            turns++;
+    
+    // function player ($firebase) {
+    //     var player = function(playerName) {
+    //         var ref = new Firebase("https:/tictactech.firebaseio/players");
+    //         var firebasePlayer = $firebase(ref).$asObject();
 
-            if (box.owner === "") {
-                if (turns % 2 === 0) {
-                    box.owner = "X";
-                    self.board.$save(box);
-                    console.log(box);
+    //         self.firebasePlayer = firebasePlayer;
+
+    //         firebasePlayer.$loaded(function() {
+    //             createPlayers();
+    //         });
+    //         return player()
+    //     }
+
+    //     function createPlayers(){
+    //         if (firebasePlayer.playerCount===undefined) {
+    //             firebasePlayer.playerCount= 1; 
+    //             firebasePlayer.playerCount.$save(firebasePlayer);
+    //         }
+    //         else if (firebasePlayer.playerCount == 1) {
+    //             firebasePlayer.playerCount = 2
+    //             firebasePlayer.playerCount.$save(firebasePlayer);
+    //         }
+    //         else {
+    //             firebasePlayer.playerCount = undefined;
+    //         }
+    //     };
+    // }
+
+    
+    // var turns = 1;
+    function pickBox($index) {
+    
+
+            self.gamePlay.turns++;
+
+
+            // player = 1;
+
+            if (self.board[$index].owner === "") {
+                if (self.gamePlay.turns % 2 === 0) {
+                    self.board[$index].owner = "X";
+                    self.board.$save($index);
+                    self.gamePlay.$save();
+
+                    // player++
+                    // box.player.$save($index);
+                    // playerTurn();
+                    console.log($index);
                     winner();
                 }
                 else {
-                    box.owner = "O";
-                    self.board.$save(box);
-                    console.log(box);
+                    // box.player.$save($index);
+                    self.board[$index].owner = "O";
+                    self.board.$save($index);
+                    self.gamePlay.$save()
+
+                    // player--
+                    // box.player.$save(box);
+                    // playerTurn();
+                    console.log($index);
                     winner();
                 }
+
             }
             else {
                 window.alert("Now now, you know you can't have that space! Pick another!");
             }
+        // return player;
     }
     var owner;
-    var player;
+    var winner;
 
 
 // still need a way to stop the function running if a win occurs
     function winner() {
         if (self.board[0].owner == "X" && self.board[1].owner == "X" && self.board[2].owner == "X") {
-            self.player ="PLAYER ONE WINS!";
+            self.winner ="PLAYER ONE WINS!";
             console.log("player one!");
         }
         else if (self.board[3].owner == "X" && self.board[4].owner == "X" && self.board[5].owner == "X") {
-            self.player ="PLAYER ONE WINS!";
+            self.winner ="PLAYER ONE WINS!";
         }
         else if (self.board[6].owner == "X" && self.board[7].owner == "X" && self.board[8].owner == "X") {
-            self.player ="PLAYER ONE WINS!";
+            self.winner ="PLAYER ONE WINS!";
         }
         else if (self.board[0].owner == "X" && self.board[3].owner == "X" && self.board[6].owner == "X") {
-            self.player ="PLAYER ONE WINS!";
+            self.winner ="PLAYER ONE WINS!";
         }
         else if (self.board[1].owner == "X" && self.board[4].owner == "X" && self.board[7].owner == "X") {
-            self.player ="PLAYER ONE WINS!";
+            self.winner ="PLAYER ONE WINS!";
         }
         else if (self.board[2].owner == "X" && self.board[5].owner == "X" && self.board[8].owner == "X") {
-            self.player ="PLAYER ONE WINS!";
+            self.winner ="PLAYER ONE WINS!";
         }
         else if (self.board[0].owner == "X" && self.board[4].owner == "X" && self.board[8].owner == "X") {
-            self.player ="PLAYER ONE WINS!";
+            self.winner ="PLAYER ONE WINS!";
         }
         else if (self.board[2].owner == "X" && self.board[4].owner == "X" && self.board[6].owner == "X") {
-            self.player ="PLAYER ONE WINS!";
+            self.winner ="PLAYER ONE WINS!";
         }
         else if (self.board[0].owner == "O" && self.board[1].owner == "O" && self.board[2].owner == "O") {
-            self.player ="PLAYER TWO WINS!";
+            self.winner ="PLAYER TWO WINS!";
         }
         else if (self.board[3].owner == "O" && self.board[4].owner == "O" && self.board[5].owner == "O") {
-            self.player ="PLAYER TWO WINS!";
+            self.winner ="PLAYER TWO WINS!";
         }
         else if (self.board[6].owner == "O" && self.board[7].owner == "O" && self.board[8].owner == "O") {
-            self.player ="PLAYER TWO WINS!";
+            self.winner="PLAYER TWO WINS!";
         }
         else if (self.board[0].owner == "O" && self.board[3].owner == "O" && self.board[6].owner == "O") {
-            self.player ="PLAYER TWO WINS!";
+            self.winner ="PLAYER TWO WINS!";
         }
         else if (self.board[1].owner == "O" && self.board[4].owner == "O" && self.board[7].owner == "O") {
-            self.player ="PLAYER TWO WINS!";
+            self.winner ="PLAYER TWO WINS!";
         }
         else if (self.board[2].owner == "O" && self.board[5].owner == "O" && self.board[8].owner == "O") {
-            self.player ="PLAYER TWO WINS!";
+            self.winner ="PLAYER TWO WINS!";
         }
         else if (self.board[0].owner == "O" && self.board[4].owner == "O" && self.board[8].owner == "O") {
-            self.player ="PLAYER TWO WINS!";
+            self.winner ="PLAYER TWO WINS!";
         }
         else if (self.board[2].owner == "O" && self.board[4].owner == "O" && self.board[6].owner == "O") {
-            self.player ="PLAYER TWO WINS!";
+            self.winner ="PLAYER TWO WINS!";
         }
-        else if(turns == 10){
-            self.player= "It's a tie! Wow!";
+        else if(self.gamePlay.turns == 10){
+            self.winner= "It's a tie! Wow!";
         }
-        else if (turns % 2 == 0) {
-            self.player = "PLAYER TWO, PICK A BOX!";
+        else if (self.gamePlay.turns % 2 == 0) {
+            self.winner = "PLAYER TWO, PICK A BOX!";
         } else {
-            self.player = "PLAYER ONE, PICK A BOX";
+            self.winner = "PLAYER ONE, PICK A BOX";
         }
-        return self.player;
+        return self.winner;
     }
 
 
-
+// for loop this 
     function clearBoard() {
         self.board[0].owner = "";
         self.board[1].owner = "";
@@ -282,6 +273,10 @@ function TicController($firebase) {
         self.board[6].owner = "";
         self.board[7].owner = "";
         self.board[8].owner = "";
+        self.gamePlay.turns= 1;
+        self.board.$save();
+        self.gamePlay.$save();
+        console.log("reset");
     }
 
 
